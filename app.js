cd0a25cb989fdebaa87baa015f148956ee2c9749
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-03-15 03:13:05 +0100

    Add: more changes, but still to finish

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 //pacchetti da importare
+import connection from "./data/movie_db.js";
 import express from 'express';
 
 //impostiamo express e la porta del server
 const app = express()
-const port = 3000
+const port = process.env.SERVER_PORT || 3000
+
+import movieRouter from './routes/movieRouter.js'
+
+app.use( '/movie' , movieRouter)
+
+app.use(express.json())
+
+app.get( '/', (req, res) => {
+    res.send( 'Server Movie tutto a posto')
+})
 
 //attivazione del server
 app.listen(port, () => {
---
+++ routes/movieRouter.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import { index, show, destroy } from '../controllers/movieController'
+import movieController from '../controllers/movieController'
+
+const router = express()
+
+//index
+//localhost:3000/api/movies
+router.get('/', index);
+
+//show
+//localhost:3000/api/movies/:id
+router.get('/:id', show);
+
+//destroy
+//localhost:3000/api/movies/:id
+router.delete('/:id', destroy);
+
+export default router
